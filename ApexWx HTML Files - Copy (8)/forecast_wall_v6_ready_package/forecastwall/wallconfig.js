c12cb18d289d3c6046dd061953ed28ca6c1aa441
ApexWx <66037124+ApexWx@users.noreply.github.com>
2026-08-28 10:30:37 -0400

    update

---
+++ ApexWx HTML Files - Copy (8)/forecast_wall_v6_ready_package/forecastwall/wallconfig.js
@@ -0,0 +1,23 @@
+const WALLS = [
+
+{id:"wall-a",name:"Wall-A"},
+{id:"wall-f",name:"Wall-F"},
+{id:"wall-ne",name:"Wall-NE"},
+{id:"aloft",name:"Aloft"},
+{id:"climate",name:"Climate"},
+{id:"gis",name:"GIS"},
+{id:"maps",name:"Maps"},
+{id:"marine",name:"Marine"},
+{id:"models",name:"Models"},
+{id:"nws",name:"NWS"},
+{id:"radar",name:"Radar"},
+{id:"rain",name:"Rain"},
+{id:"satellite",name:"Satellite"},
+{id:"snow",name:"Snow"},
+{id:"surface",name:"Surface"},
+{id:"temps",name:"Temps"},
+{id:"thunder",name:"Thunder"},
+{id:"tropical",name:"Tropical"},
+{id:"wind",name:"Wind"}
+
+];
---
+++ ApexWx HTML Files - Copy (8)/forecast_wall_v6_ready_package/forecastwall/walls/tropical.js
@@ -0,0 +1,10 @@
+
+buildWall(
+"Tropical Wall",
+[
+["NHC Atlantic Outlook",
+"https://www.nhc.noaa.gov/xgtwo/two_atl_5d0.png",
+"Atlantic Satellite",
+"https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/taw/GEOCOLOR/latest.jpg"]
+]
+);
